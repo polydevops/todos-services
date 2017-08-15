2600e03a22f38b77cb907e59513978cf489078dc
Connor Pauly <cp1992@gmail.com>
2017-08-14 19:16:31 -0700

    Fixed up updateTodo which somehow had broken

--- todo/todo-item-controller.js
+++ todo/todo-item-controller.js
@@ -22,7 +22,7 @@ controller.createTodo = function(req, res, next) {
 
 controller.updateTodo = function(req, res, next) {
   service
-  .updateItem(req.body.todosId, req.body.todo)
+  .updateItem(req.params.id, req.body.todo)
   .then(success => {
     if (success) {
       res.status(200).end();
